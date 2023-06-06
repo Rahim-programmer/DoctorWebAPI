eb432807a3369b9c083acf5629a33cbc0c0c8c0f
RahimAtabaev <rahimatabaev75@mail.ru>
2023-06-07 01:11:02 +0600

    final project

--- src/test/java/com/example/DoctorWebAPI/DoctorWebApiApplicationTests.java
+++ src/test/java/com/example/DoctorWebAPI/DoctorWebApiApplicationTests.java
@@ -1,13 +1,32 @@
 package com.example.DoctorWebAPI;
 
+import com.example.DoctorWebAPI.controller.ArticlesController;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@SpringBootTest
+@WebMvcTest(ArticlesController.class)
 class DoctorWebApiApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void testArticles() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/Articles"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("Articles"));
 	}
 
+	@Test
+	public void testIndex() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/"))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.view().name("index"));
+	}
 }
+
+
